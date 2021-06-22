932036f2b5b03ea1d57fa44f268db3e0096a9aed
ptrkc <40273827+ptrkc@users.noreply.github.com>
2021-06-22 12:36:17 -0300

    Started routes and GlobalStyle

--- src/App.js
+++ src/App.js
@@ -1,25 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import SignIn from "./SignIn";
+import GlobalStyle from "./styles/GlobalStyle";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <Router>
+            <GlobalStyle />
+            <Switch>
+                <Route path="/sign-in" exact>
+                    <SignIn />
+                </Route>
+                {/* <Route path="/sign-up" exact>
+                    <SignUp />
+                </Route>
+                <Route path="/home" exact>
+                    <Home />
+                </Route>
+                <Route path="/add-in" exact>
+                    <AddIn />
+                </Route>
+                <Route path="/add-out" exact>
+                    <AddOut />
+                </Route> */}
+            </Switch>
+        </Router>
+    );
 }
 
 export default App;
---
+++ src/styles/GlobalStyle.js
@@ -0,0 +1,17 @@
+import { createGlobalStyle } from "styled-components";
+
+const GlobalStyle = createGlobalStyle`
+  body {
+    background-color: #8C11BE;
+    color: #fff;
+    font-family: 'Raleway', sans-serif;
+  }
+  h1 {
+    font-family: 'Saira Stencil One', cursive;
+  }
+  button {
+    background-color: #A328D6;
+    border-radius: 5px;
+  }
+`;
+export default GlobalStyle;
